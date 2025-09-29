6a76039dcfa87b61d7cc5b43171ae387c4c411e8
Erwin67000 <philae.menuiserie@gmail.com>
2025-09-29 14:30:45 +0200

    Debug2

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Accueil = () => {
     return (
         <main>
-            <h1>Bienvenue sur le nouveau site Philae Design</h1>
+            <h1>Bienvenue sur Philae Design</h1>
             <p>Découvrez nos créations personnalisées en 3D.</p>
             {/* Ajoute des images ou teasers ici */}
         </main>
