2b5a5e0e43aad9d635f3fa1d158164880a5b9e62
Erwin67000 <philae.menuiserie@gmail.com>
2025-09-30 07:06:13 +0200

    Update Configurateur.js

--- src/pages/Configurateur.js
+++ src/pages/Configurateur.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Plot from 'react-plotly.js';
 import { computeGeometry } from './geometry3D';
 
