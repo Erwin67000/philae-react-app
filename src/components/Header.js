1a6098d136a4a4212fcbbfe747d78ea69f0aa6de
Erwin67000 <philae.menuiserie@gmail.com>
2025-10-04 10:52:55 +0200

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ const Header = () => {
             <img src="/assets/logo.jpg" alt="Logo Philae Design" style={{ height: '80px' }} />
             <nav>
                 <ul style={{ listStyle: 'none', padding: 0 }}>
-                    <li style={{ display: 'inline', margin: '0 15px' }}><Link to="/">Accueil</Link></li>
-                    <li style={{ display: 'inline', margin: '0 15px' }}><Link to="/configurateur">Configurateur</Link></li>
-                    <li style={{ display: 'inline', margin: '0 15px' }}><Link to="/boutique">Boutique</Link></li>
-                    <li style={{ display: 'inline', margin: '0 15px' }}><Link to="/contact">Contact</Link></li>
+                    <li style={{ display: 'inline', margin: '0 80px' }}><Link to="/">Accueil</Link></li>
+                    <li style={{ display: 'inline', margin: '0 80px' }}><Link to="/configurateur">Configurateur</Link></li>
+                    <li style={{ display: 'inline', margin: '0 80px' }}><Link to="/boutique">Boutique</Link></li>
+                    <li style={{ display: 'inline', margin: '0 80px' }}><Link to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
