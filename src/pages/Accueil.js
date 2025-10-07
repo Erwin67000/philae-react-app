9e36efc0d0f9eece3a83f7d2acedd8869dcf5a0a
Erwin67000 <philae.menuiserie@gmail.com>
2025-10-07 17:08:11 +0200

    Update Accueil.js

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -5,6 +5,7 @@ const Accueil = () => {
         <main>
             <h1>Bienvenue sur le nouveau site web de Philae Design</h1>
             <p>Site en constuction.</p>
+            <p>Des lignes simples pour une beauté universelle. La forme et la fonction réunies dans un meuble unique. Philae naît de la géométrie intemporelle, où chaque arête, identique et précise, s’assemble en un design qui transcende les frontières. Fabriqué avec du bois local, chaque pièce célèbre l’authenticité des matériaux naturels tout en offrant une modularité infinie. Créez, configurez, personnalisez – du sommet jusqu’à la base, votre style prend vie avec une facilité intuitive. Que ce soit une table, une étagère ou une œuvre sur mesure, Philae transforme des contraintes parfaites en créations qui vous ressemblent, sans limite autre que votre imagination.</p>
             <img src="/assets/logo_PHILAE.jpg" alt="Logo Philae Design" style={{ height: '280px' }} />
             {/* Ajoute des images ou teasers ici */}
         </main>
